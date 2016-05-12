f758f2f5bf676b8358b9d804a50257ff783cb074
minsyslvi <lvi@minsys.io>
2016-05-13 00:55:27 +0200

    Minor Debugging

--- launchpad/scw-postinstall.sh
+++ launchpad/scw-postinstall.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+cd /home/ansible/ansible.cloud.minsys.io.git/
+svn update
+cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
 ansible-playbook /etc/ansible/playbooks/setup_postinstall-scw.yaml
 exit 
