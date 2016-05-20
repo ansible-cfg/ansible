a30c44b1b80370e88c409987959a5a1ae25fdfe0
minsyslvi <lvi@minsys.io>
2016-05-21 00:38:00 +0200

    Improved Playbooks 2

--- launchpad/install_new_host-scw.sh
+++ launchpad/install_new_host-scw.sh
@@ -2,6 +2,8 @@
 cd /home/ansible/ansible.cloud.minsys.io.git/
 svn update
 cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
+:> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_base_OS-scw.yaml
 ansible-playbook /etc/ansible/playbooks/install_sensu-client-scw.yaml
+ansible-playbook /etc/ansible/playbooks/install_docker.yaml
 exit 
