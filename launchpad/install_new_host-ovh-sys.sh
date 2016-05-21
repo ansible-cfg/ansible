99cdf907aaac0efb4769c64f6fc529438e56a9f0
minsyslvi <lvi@minsys.io>
2016-05-21 20:29:02 +0200

    Test 3 install_ovh + ovh-sys

--- launchpad/install_new_host-ovh-sys.sh
+++ launchpad/install_new_host-ovh-sys.sh
@@ -4,8 +4,6 @@ svn update
 cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
 :> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_base_OS-ovh.yaml
-:> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_sensu-client-ovh.yaml
-:> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_docker.yaml
 exit
--- launchpad/install_new_host-scw.sh
+++ launchpad/install_new_host-scw.sh
@@ -4,8 +4,6 @@ svn update
 cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
 :> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_base_OS-scw.yaml
-:> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_sensu-client-scw.yaml
-:> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_docker.yaml
 exit 
