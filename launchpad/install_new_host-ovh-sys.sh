25150c5ae3e75e3a32592266110bf33a5d70de58
minsyslvi <lvi@minsys.io>
2016-05-21 20:43:29 +0200

    Test 5 install_ovh + ovh-sys

--- launchpad/install_new_host-ovh-sys.sh
+++ launchpad/install_new_host-ovh-sys.sh
@@ -5,5 +5,6 @@ cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
 :> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_base_OS-ovh.yaml
 ansible-playbook /etc/ansible/playbooks/install_sensu-client-ovh.yaml
+:> /root/.ssh/known_hosts
 ansible-playbook /etc/ansible/playbooks/install_docker-ovh.yaml
 exit
