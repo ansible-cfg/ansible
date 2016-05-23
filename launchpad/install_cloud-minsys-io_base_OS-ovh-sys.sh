0ba7a905106addd6f057c6673f3ece1501d86d73
minsyslvi <lvi@minsys.io>
2016-05-23 17:12:51 +0200

    Creating a whole playbook for ovh-sys

---
+++ launchpad/install_cloud-minsys-io_base_OS-ovh-sys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/ansible/ansible.cloud.minsys.io.git/
+svn update
+cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
+:> /root/.ssh/known_hosts
+ansible-playbook /etc/ansible/playbooks/install_cloud-minsys-io_base_OS-ovh-sys.yaml
+exit
