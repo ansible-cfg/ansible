94f2eb238d447bf2f17a14156b0bd8a497bdfa73
minsyslvi <lvi@minsys.io>
2016-05-25 00:38:25 +0200

    Playbook for scw managed by rancher - test 4

--- launchpad/test-rancher.sh
+++ launchpad/test-rancher.sh
@@ -4,6 +4,6 @@ svn update
 cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
 :> /root/.ssh/known_hosts
 read -p 'Enter the new minion ip adress  : ' ip_minion
-echo -e "[scw_newinstances]\n$ip_minion " > /etc/ansible/inventory/hosts 
+echo -e "[scw_newinstances]\n$ip_minion\n[scw_newinstances:vars]\nhost_key_checking = False\nansible_ssh_user= root\nansible_ssh_private_key_file= /root/.ssh/scw-root_rsa " > /etc/ansible/inventory/hosts 
 ansible-playbook /etc/ansible/playbooks/testing-rancher.yaml
 exit
