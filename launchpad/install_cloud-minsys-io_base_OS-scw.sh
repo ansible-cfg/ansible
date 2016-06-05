0c3dfde132c3e847ca342d1d9620308acc05f3de
minsyslvi <lvi@minsys.io>
2016-06-05 21:49:21 +0200

    Playbook for scw - 1

--- launchpad/install_cloud-minsys-io_base_OS-scw.sh
+++ launchpad/install_cloud-minsys-io_base_OS-scw.sh
@@ -3,5 +3,7 @@ cd /home/ansible/ansible.cloud.minsys.io.git/
 svn update
 cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
 :> /root/.ssh/known_hosts
+read -p 'Enter the new minion ip adress  : ' ip_minion
+echo -e "[scw_newinstances]\n$ip_minion\n[scw_newinstances:vars]\nhost_key_checking = False\nansible_ssh_user= root\nansible_ssh_private_key_file= /root/.ssh/scw-root_rsa " > /etc/ansible/inventory/hosts 
 ansible-playbook /etc/ansible/playbooks/install_cloud-minsys-io_base_OS-scw.yaml
 exit
