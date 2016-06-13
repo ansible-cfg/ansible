296debad6af94193ce4a09ed5ab219667cc49efd
minsyslvi <lvi@minsys.io>
2016-06-13 19:14:48 +0200

    Added Elasticsearch/Logstash rancher

--- launchpad/newhost/scw/rancher_newhost_logstash.sh
+++ launchpad/newhost/scw/rancher_newhost_logstash.sh
@@ -3,7 +3,7 @@ cd /home/ansible/ansible.cloud.minsys.io.git/
 svn update
 cp -R /home/ansible/ansible.cloud.minsys.io.git/trunk/* /etc/ansible/
 :> /root/.ssh/known_hosts
-read -p 'Enter the new host ip adress managed by rancher, elasticsearch : ' ip_minion
+read -p 'Enter the new host ip adress managed by rancher, logstash : ' ip_minion
 echo -e "[scw_newhost_base]\n$ip_minion\n[scw_newhost_base:vars]\nhost_key_checking = False\nansible_ssh_user= root\nansible_ssh_private_key_file= /root/.ssh/scw-root_rsa " > /etc/ansible/inventory/hosts 
-ansible-playbook /etc/ansible/playbooks/newhost/scw/rancher_newhost_elasticsearch.yaml
+ansible-playbook /etc/ansible/playbooks/newhost/scw/rancher_newhost_logstash.yaml
 exit
